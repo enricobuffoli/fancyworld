92a158049f9592794681c2fda89967e7943f2bfd
enricobuffoli <enrico.buffoli1994@gmail.com>
2017-10-11 13:54:00 +0200

    Add files via upload

---
+++ FancyWorld2.0/src/injected/it/fancyworld/Gioco/Luoghi/LuoghiConChiave/Pass.java
@@ -0,0 +1,13 @@
+package injected.it.fancyworld.Gioco.Luoghi.LuoghiConChiave;
+
+import injected.it.fancyworld.Gioco.Luoghi.Luogo;
+
+/**
+ * Created by enrico on 05/10/17.
+ */
+public class Pass extends LuogoChiave
+{
+    public Pass(int x, int y, String name) {
+        super(x, y, name, "P");
+    }
+}
---
+++ FancyWorld2.0/src/injected/it/fancyworld/Gioco/Luoghi/Normal.java
@@ -0,0 +1,13 @@
+package injected.it.fancyworld.Gioco.Luoghi;
+
+import injected.it.fancyworld.Gioco.Luoghi.Luogo;
+
+/**
+ * Created by enrico on 05/10/17.
+ */
+public class Normal extends Luogo {
+
+    public Normal(int x, int y, String name) {
+        super(x, y, name, "N");
+    }
+}
