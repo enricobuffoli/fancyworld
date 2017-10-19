209cc34a7b8b565e1daf83f92612847c5c76ee8a
andreSela <32704089+andreSela@users.noreply.github.com>
2017-10-19 17:10:04 +0200

    Add files via upload

---
+++ FancyWorld 1.0/src/injected/it/fancyworld/Gioco/Luoghi/Pass.java
@@ -0,0 +1,14 @@
+package injected.it.fancyworld.Gioco.Luoghi;
+
+import injected.it.fancyworld.Gioco.Luoghi.Luogo;
+
+/**
+ * Created by enrico on 05/10/17.
+ */
+public class Pass extends Luogo
+{
+
+    public Pass(int x, int y, String name) {
+        super(x, y, name, "P");
+    }
+}
---
+++ FancyWorld 1.0/src/injected/it/fancyworld/Main.java
@@ -0,0 +1,19 @@
+package injected.it.fancyworld;
+
+import injected.it.fancyworld.Gioco.Gioco;
+import injected.it.fancyworld.Utils.Utility;
+
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Utility.readPlacesFromFile();
+
+        Gioco gioco = Gioco.getGioco();
+
+    }
+
+}
