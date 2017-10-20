15535fa309d666c4a91512b9c7094042b0380431
andreSela <32704089+andreSela@users.noreply.github.com>
2017-10-20 13:14:08 +0200

    Add files via upload

---
+++ FancyWorld 2.0/src/injected/it/fancyworld/Gioco/Chiave.java
@@ -0,0 +1,24 @@
+package injected.it.fancyworld.Gioco;
+
+/**
+ * Created by enrico on 11/10/17.
+ */
+public class Chiave {
+
+    private String tipo;
+    private String nome;
+
+    public Chiave(String tipo)
+    {
+        this.tipo = tipo;
+    }
+
+    public String toString() {
+        return nome;
+    }
+
+    public Chiave clone()
+    {
+        return new Chiave(tipo);
+    }
+}
